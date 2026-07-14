064a143ec70d958edf2a1a1c3a14a6c0654086d9
sujalchaundkar-afk <sujalchaundkar@gmail.com>
2026-07-14 21:04:25 +0530

    app.py

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
 
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return """
+    <h1>AI Career Mentor</h1>
+    <p>Backend is running successfully.</p>
+    <p>Frontend page will be connected later.</p>
+    """
+
+if __name__ == "__main__":
+    app.run(debug=True)
