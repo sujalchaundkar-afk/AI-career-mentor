7e6248ff21a2d66a0b04dbf00a79d9dbe7896d57
sujalchaundkar-afk <sujalchaundkar@gmail.com>
2026-07-15 18:34:11 +0530

    Connect Flask API

--- app.py
+++ app.py
@@ -1,15 +1,25 @@
-
-from flask import Flask, render_template
+from flask import Flask, request, jsonify
+from career import get_career
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return """
-    <h1>AI Career Mentor</h1>
-    <p>Backend is running successfully.</p>
-    <p>Frontend page will be connected later.</p>
-    """
+    return "AI Career Mentor Backend is Running"
+
+@app.route("/career", methods=["POST"])
+def career():
+
+    data = request.get_json()
+
+    skills = data["skills"]
+    interest = data["interest"]
+
+    answer = get_career(skills, interest)
+
+    return jsonify({
+        "career": answer
+    })
 
 if __name__ == "__main__":
     app.run(debug=True)
