3517fd2f6c9c639c0efb4eb2f6e97b018329b324
sujalchaundkar-afk <sujalchaundkar@gmail.com>
2026-07-15 18:45:03 +0530

    career.py

--- career.py
+++ career.py
@@ -3,10 +3,10 @@ def get_career(skills, interest):
     skills = skills.lower()
     interest = interest.lower()
 
-    if "python" in skills or "ai" in interest:
+    if "python" in skills or "artificial intelligence" in interest or "ai" in interest:
         return "AI / Machine Learning Engineer"
 
-    elif "html" in skills or "css" in skills:
+    elif "html" in skills or "css" in skills or "javascript" in skills:
         return "Frontend Web Developer"
 
     elif "java" in skills:
@@ -18,5 +18,14 @@ def get_career(skills, interest):
     elif "c" in skills or "c++" in skills:
         return "Software Developer"
 
+    elif "data" in interest:
+        return "Data Analyst"
+
+    elif "cyber" in interest:
+        return "Cyber Security Analyst"
+
+    elif "cloud" in interest:
+        return "Cloud Engineer"
+
     else:
-        return "Keep learning new skills and explore different career options."
+        return "Sorry, I could not find a matching career. Try adding more skills."
